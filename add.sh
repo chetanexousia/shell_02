ac9b19601c9eb2d7c95d0a183f310aa6855b771b
chetanexousia <chetan.exousia45@gmail.com>
2024-08-28 15:16:23 +0530

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Static numbers to add
+# Static numbers to substract
 num1=10
 num2=5
 
-# Perform the addition
-sum=$((num1 + num2))
+# Perform the substraction
+sum=$((num1 - num2))
 
 # Display the result
-echo "The sum of $num1 and $num2 is: $sum"
+echo "The substract of $num1 and $num2 is: $sum"
